d174a8cb3957218c7ea4cf78e65b5a3bc6dc47d3
callmerizal <72860501+callmerizal@users.noreply.github.com>
2020-10-14 16:35:23 +0700

    Add files via upload

---
+++ kontrolIF_Else.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package random2;
+
+/**
+ *
+ * @author M.T.RIZAL
+ */
+public class kontrolIF_Else {
+    public static void main(String[] args) {
+        int totalbelanja = 100000;
+        int jmlh_brg_dibeli = 2;
+        double diskon = 0.25; 
+        double hrg_brg_stlh_diskon;
+        
+        if (totalbelanja>50000 &&  jmlh_brg_dibeli>1)
+        {hrg_brg_stlh_diskon = totalbelanja-(totalbelanja*diskon);
+            System.out.println("Harga barang setelah diskon :" +hrg_brg_stlh_diskon);
+        
+        
+        }
+        else { System.out.println("Maaf anda belum dapat diskon");
+        
+         
+        }
+            
+    }
+}
